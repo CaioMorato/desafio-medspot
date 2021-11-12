0fd22bbc3f2cd04b3d3d2f606c28e638209d78f7
Caio Morato <morato_dev@protonmail.com>
2021-11-11 21:24:03 -0300

    estrutura base header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,17 @@ import medspotLogo from '../images/medspot-logo.png'
 
 function Header() {
   return (
-    <header id="header" className="flex items-center justify-between">
+    <header id="header" className="flex items-center justify-between mx-5">
       <div className="logo-container">
         <img src={medspotLogo} width="64px" alt="Logo da empresa Medspot" />
       </div>
       <div className="company-name">Medspot</div>
       <div className="navigation-container">
-        <ul className="flex">
+        <ul id="menu" className="flex flex-col md:flex-row md:gap-x-10">
           <li>
-            Marcar consulta
+            Marcar
           </li>
-          <li>Visualizar consulta</li>
+          <li>Visualizar</li>
         </ul>
       </div>
     </header>
