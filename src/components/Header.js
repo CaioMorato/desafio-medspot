0af60104917f7fbf73feb2b73acf96ec397a4188
Caio Morato <morato_dev@protonmail.com>
2021-11-15 01:52:12 -0300

    adição toggle pra darktheme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 // vitals
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+// context
+import MyContext from '../context/MyContext';
 // styles
 import medspotLogo from '../images/medspot-no-bg.png';
-import { BsList } from 'react-icons/bs';
+import { BsList, BsSunFill, BsFillMoonFill } from 'react-icons/bs';
 
 function Header() {
   const [showMenu, setShowMenu] = useState(false);
+  const { darkTheme, setDarkTheme } = useContext(MyContext);
   return (
     <header id="header" className="flex items-center justify-between mx-4 mb-5">
-      <div className="logo-container md:w-40">
+      <div className="logo-container md:w-52">
         <img
           src={medspotLogo}
           width="64px"
@@ -16,11 +19,11 @@ function Header() {
           className=""
         />
       </div>
-      <div className="company-name md:w-40">
+      <div className="company-name md:w-52">
         <h1 className="text-2xl font-bold text-center text-blue-dark">Medspot</h1>
       </div>
-      <div className="navigation-container md:w-40">
-        <nav id="nav">
+      <div className="navigation-container">
+        <nav id="nav" className="flex flex-row items-center">
           <button type="button" onClick={() => setShowMenu(!showMenu)}>
             <BsList size="2rem" />
           </button>
@@ -32,6 +35,19 @@ function Header() {
               <a href="#visualizar">Visualizar</a>
             </li>
           </ul>
+          <div className="toggle-container ml-6">
+            <input
+              type="checkbox"
+              class="toggle-checkbox"
+              id="chk"
+              onClick={() => setDarkTheme(!darkTheme)}
+            />
+            <label className="toggle-label" for="chk">
+              <BsSunFill fill="#f39c12" size="10px" />
+              <BsFillMoonFill fill="#f1c40f" size="10px" />
+              <div className="toggle-ball"></div>
+            </label>
+          </div>
         </nav>
       </div>
     </header>
