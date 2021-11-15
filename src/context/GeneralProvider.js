4fdcc3f5e086fffbcc081cede75a07082c9e1be8
Caio Morato <morato_dev@protonmail.com>
2021-11-15 01:53:52 -0300

    setando darktheme no context provider

--- src/context/GeneralProvider.js
+++ src/context/GeneralProvider.js
@@ -6,13 +6,15 @@ import MyContext from './MyContext';
 function GeneralProvider({ children }) {
   const [scheduledTime, setScheduledTime] = useState([]);
   const [canceledSchedule, setCanceledSchedule] = useState([]);
-
+  const [darkTheme, setDarkTheme] = useState(false);
 
   const globalState = {
     scheduledTime,
     setScheduledTime,
     canceledSchedule,
     setCanceledSchedule,
+    darkTheme,
+    setDarkTheme,
   };
 
   return <MyContext.Provider value={globalState}>{children}</MyContext.Provider>;
